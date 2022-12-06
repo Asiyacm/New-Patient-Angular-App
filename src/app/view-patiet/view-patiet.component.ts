956efbc3bb56f2636db20a8d4edf5cb03a604c55
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-07 03:06:20 +0530

    view commit

---
+++ src/app/view-patiet/view-patiet.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-patiet',
+  templateUrl: './view-patiet.component.html',
+  styleUrls: ['./view-patiet.component.css']
+})
+export class ViewPatietComponent {
+
+
+  
+  patient:any=[]
+
+  
+}
